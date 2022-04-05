7c8a26f0fe60dd90dde584d707b7b4dd895476e9
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-04-05 11:21:52 +0200

    9.5

--- index.ts
+++ index.ts
@@ -1,11 +1,26 @@
 import express from 'express'
+import calculateBmi from './util/calculateBmi'
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
+  if (!weight || !height || isNaN(weight) || isNaN(height)) {
+    return res.status(400).json({ error: 'malformatted parameters' })
+  } else {
+    return res.status(202).json({
+      weight: weight,
+      height: height,
+      bmi: calculateBmi(height, weight)
+    })
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on pot ${PORT}`);
---
+++ util/calculateBmi.ts
@@ -0,0 +1,25 @@
+const calculateBmi = (a: number, b: number): string => {
+  if (a > 3) a = a / 100; // turn to m if cm
+
+  const calculator = (): number => {
+    return b / (a ** 2);
+  };
+
+  if (calculator() < 16.0) {
+    return 'Underweight (Severe thinness)';
+  } else if (calculator() > 15.9 && calculator() < 17.0) {
+    return 'Underweight (Moderate thinness)';
+  } else if (calculator() > 16.9 && calculator() < 18.5) {
+    return 'Underweight (Mild thinness)';
+  } else if (calculator() > 18.4 && calculator() < 25.0) {
+    return 'Normal (healthy weight)';
+  } else if (calculator() > 24.9 && calculator() < 30.0) {
+    return 'Overweight (Pre-obese)'
+  } else if (calculator() > 29.9) {
+    return 'OBESE'
+  } else {
+    return 'IDK what I\'m doing'
+  };
+};
+
+export default calculateBmi
